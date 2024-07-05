0c7cbf273608cb3e69c324b933a789339103470e
sedefb <sedefbozkurt.4379@gmail.com>
2024-07-05 11:29:25 +0300

    feat: Hesap geçmişini görüntüleme eklendi

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Kullanıcıların bakiyeyi kontrol etmesine, para yatırmasına ve para çekmesi
 - Bakiye kontrolü
 - Para yatırma
 - Para çekme
+
+## Gereksinimler
+- Python 3.x
+
+## Kurulum
+1. Repoyu klonlayın:
+  git clone https://github.com/sedefbozkurt/atmApp.git
--- atmApp.py
+++ atmApp.py
@@ -5,17 +5,19 @@ def showMenu():
   print("1. Bakiyeni Kontrol Et")
   print("2. Para Yatır")
   print("3. Para Çek")
-  print("4. Çıkış")
+  print("4. Hesap Geçmişi")
+  print("5. Çıkış")
 
 def CheckBalance(balance):
   print(f"Mevcut bakiyeniz: {balance:.2f} ₺")
 
-def depositMoney(balance):
+def depositMoney(balance, history):
     while True:
         try:
             amount = float(input("Yatırılacak tutarı girin: "))
             if amount > 0: 
                 balance += amount
+                history.append(f"{amount} ₺ yatırıldı.")
                 print(f"{amount} ₺ yatırıldı.")
                 break
             else:
@@ -24,13 +26,14 @@ def depositMoney(balance):
             print("Geçersiz giriş. Lütfen sayısal bir değer giriniz.")
     return balance
 
-def withdrawMoney(balance):
+def withdrawMoney(balance, history):
     while True:
         try:
             amount = float(input("Çekilecek tutarı girin: "))
             if amount > 0:
                 if balance >= amount:
                     balance -= amount
+                    history.append(f"{amount} ₺ çekildi.")
                     print(f"{amount:.2f} ₺ çekildi")
                     break
                 else:
@@ -40,21 +43,29 @@ def withdrawMoney(balance):
         except ValueError:
             print("Geçersiz giriş. Lütfen sayısal bir değer girin.")
     return balance
+
+def showHistory(history):
+    print("\nHesap Geçmişi:")
+    for record in history:
+        print(record)
           
 def main():
     balance = 0.0 # İlk bakiye
+    history = []
 
     while True:
         showMenu()
-        choice = input("Yapmak istediğiniz işlemi seçiniz (1-4): ")
+        choice = input("Yapmak istediğiniz işlemi seçiniz (1-5): ")
 
         if choice == '1': 
             CheckBalance(balance)
         elif choice == '2':
-            balance = depositMoney(balance)
+            balance = depositMoney(balance, history)
         elif choice == '3':
-            balance = withdrawMoney(balance)
+            balance = withdrawMoney(balance, history)
         elif choice == '4':
+            showHistory(history)
+        elif choice == '5':
             print("Çıkış yapılıyor...")
             break
         else:
